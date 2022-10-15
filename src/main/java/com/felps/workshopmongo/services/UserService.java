a0cb5c63aefc3e89016ff0f46207083bf72b260b
Fulano de Tal <felipesand824@gmail.com>
2022-10-15 20:35:57 -0300

    Obtendo um usuário por id

--- src/main/java/com/felps/workshopmongo/services/UserService.java
+++ src/main/java/com/felps/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.felps.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felps.workshopmongo.domain.User;
 import com.felps.workshopmongo.repositories.UserRepository;
+import com.felps.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,6 +20,12 @@ public class UserService {
 		List<User> users = repository.findAll();
 		
 		return users;
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj  = repository.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
---
+++ src/main/java/com/felps/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.felps.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String message) {
+		super(message);
+	}
+
+}
