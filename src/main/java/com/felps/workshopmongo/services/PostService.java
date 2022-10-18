44ff2f9454134f075733e5a5b50fd838428e233d
Fulano de Tal <felipesand824@gmail.com>
2022-10-18 15:20:41 -0300

    Obtendo um post por id

---
+++ src/main/java/com/felps/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.felps.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felps.workshopmongo.domain.Post;
+import com.felps.workshopmongo.repositories.PostRepository;
+import com.felps.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+
+}
