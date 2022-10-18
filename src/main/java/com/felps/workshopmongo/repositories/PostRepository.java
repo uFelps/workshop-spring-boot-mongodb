1e2722e267d280c20c384183746a5dcf2ec09fdc
Fulano de Tal <felipesand824@gmail.com>
2022-10-18 20:31:42 -0300

    Consulta simples com query methods

--- src/main/java/com/felps/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/felps/workshopmongo/repositories/PostRepository.java
@@ -1,9 +1,13 @@
 package com.felps.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.felps.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/felps/workshopmongo/services/PostService.java
+++ src/main/java/com/felps/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.felps.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class PostService {
 		Optional<Post> obj = repository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 
 }
