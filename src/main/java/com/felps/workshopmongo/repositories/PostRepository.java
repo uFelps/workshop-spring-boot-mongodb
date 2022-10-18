c12190d6efd13c8e06774af3da857255e6600702
Fulano de Tal <felipesand824@gmail.com>
2022-10-18 20:49:14 -0300

    Consulta simples com @Query

--- src/main/java/com/felps/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/felps/workshopmongo/repositories/PostRepository.java
@@ -3,11 +3,15 @@ package com.felps.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.felps.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': {$regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
 
 }
